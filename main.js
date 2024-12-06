64e4f384d484dc6794efb868d2ac22b69c6215d9
Anna-Kashyra <anna.kashyra.gmail.com>
2024-12-06 11:39:58 +0200

    Homework 2

--- main.js
+++ main.js
@@ -1,17 +1,36 @@
-console.log('-----main.js------');
-
-const readline = require('node:readline');
+const path = require('node:path');
+const fs = require('node:fs/promises');
 
 const foo = async () => {
-    const rlInstance = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout
-    })
+    const baseFolder = path.join(process.cwd(), 'baseFolder');
 
-    rlInstance.question('What is your name?', (name) => {
-        console.log(`Hello ${name}`);
-        rlInstance.close();
-    })
+    for (let i = 1; i <= 5; i++) {
+        const subFolder = path.join(baseFolder, `folder${i}`);
+        await fs.mkdir(subFolder, { recursive: true });
+
+        for (let j = 1; j <= 5; j++) {
+            const filePath = path.join(subFolder, `file${j}.txt`);
+            await fs.writeFile(filePath, `This is file ${j} in folder ${i}`);
+        }
+    }
+
+    async function printFolderContents(folderPath) {
+        const contents = await fs.readdir(folderPath, { withFileTypes: true });
+
+        for (const item of contents) {
+            const itemPath = path.join(folderPath, item.name);
+            const isDirectory = item.isDirectory();
+
+            console.log(`${itemPath} - ${isDirectory ? 'Folder' : 'File'}`);
+
+            if (isDirectory) {
+                await printFolderContents(itemPath);
+            }
+        }
+    }
+
+    await printFolderContents(baseFolder);
 }
 
 void foo();
+
