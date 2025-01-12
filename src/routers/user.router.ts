c736792065af8ed34c3b97c05950e094c70d2806
Anna-Kashyra <anna.kashyra.gmail.com>
2025-01-13 00:52:00 +0200

    Homework 10

--- src/routers/user.router.ts
+++ src/routers/user.router.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 
+import { AvatarConfig } from "../constants/image.constant";
 import { userController } from "../controllers/user.controller";
 import { authMiddleware } from "../middlewares/auth.middleware";
 import { commonMiddleware } from "../middlewares/common.middleware";
+import { fileMiddleware } from "../middlewares/file.middleware";
 import { UserValidator } from "../validators/user.validator";
 
 const router = Router();
@@ -18,6 +20,19 @@ router.put(
   authMiddleware.checkAccessToken,
   commonMiddleware.validateBody(UserValidator.update),
   userController.updateMe,
+);
+
+router.post(
+  "/me/avatar",
+  authMiddleware.checkAccessToken,
+  fileMiddleware.isFileValid("avatar", AvatarConfig),
+  userController.uploadAvatar,
+);
+
+router.delete(
+  "/me/avatar",
+  authMiddleware.checkAccessToken,
+  userController.deleteAvatar,
 );
 
 router.get(
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,7 +1,11 @@
+import { UploadedFile } from "express-fileupload";
+
+import { FileItemTypeEnum } from "../enums/file-item-type.enum";
 import { ApiError } from "../errors/api-error";
 import { ITokenPayload } from "../interfaces/IToken";
 import { IUser, IUserUpdateDto } from "../interfaces/IUser";
 import { userRepository } from "../repositories/user.repository";
+import { s3Service } from "./s3.service";
 
 class UserService {
   public async getList(): Promise<IUser[]> {
@@ -33,6 +37,31 @@ class UserService {
       throw new ApiError("User not found", 404);
     }
     return await userRepository.updateById(user._id, dto);
+  }
+
+  public async uploadAvatar(
+    tokenPayload: ITokenPayload,
+    file: UploadedFile,
+  ): Promise<IUser> {
+    const user = await userRepository.getById(tokenPayload.userId);
+    const avatar = await s3Service.uploadFile(
+      file,
+      FileItemTypeEnum.USER,
+      user._id,
+    );
+    const updatedUser = await userRepository.updateById(user._id, { avatar });
+    if (user.avatar) {
+      await s3Service.deleteFile(user.avatar);
+    }
+    return updatedUser;
+  }
+
+  public async deleteAvatar(tokenPayload: ITokenPayload): Promise<IUser> {
+    const user = await userRepository.getById(tokenPayload.userId);
+    await s3Service.deleteFile(user.avatar);
+    return await userRepository.updateById(user._id, {
+      avatar: null,
+    });
   }
 
   public async isEmailUnique(email: string): Promise<void> {
